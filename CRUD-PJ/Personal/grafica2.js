56d624c1a6c0b23eba2476c6a83fce0ab66f8477
cirzz-nsz <cristopherpro2021@gmail.com>
2026-03-20 16:17:46 -0600

    colores orden y forma de la grafica

--- CRUD-PJ/Personal/grafica2.js
+++ CRUD-PJ/Personal/grafica2.js
@@ -13,7 +13,6 @@ for (let contador_años = 0; contador_años < personas.length; contador_años++)
     let edad_actual = personas[contador_años].edad;
     let existe = false;
 
-    // verificar si ya existe esa edad
     for (let edad_existente = 0; edad_existente < etiquetas.length; edad_existente++) {
         if (etiquetas[edad_existente] == edad_actual + " AÑOS") {
             existe = true;
@@ -24,16 +23,15 @@ for (let contador_años = 0; contador_años < personas.length; contador_años++)
 
         let contador = 0;
 
-        // contar cuántas veces se repite esa edad
-        for (let contador_edades = 0; i < personas.length; i++) {
-            if (personas[i].edad == edad_actual) {
+        for (let contador_edades = 0; contador_edades < personas.length; contador_edades++) {
+            if (personas[contador_edades].edad == edad_actual) {
                 contador++;
             }
         }
 
         etiquetas.push(edad_actual + " AÑOS");
         data.push(contador);
-        
+
         console.log(edad_actual, "se repite:", contador);
     }
 }
@@ -43,6 +41,23 @@ let grafica = document.getElementById("myChart");
 let datos = {
     label: "estudiantes",
     data: data,
+    backgroundColor: [
+        'rgba(163,221,203,0.5)',
+        'rgba(232,233,161,0.5)',
+        'rgba(230,181,102,0.5)',
+        'rgba(229,112,126,0.5)',
+        'rgba(135,206,250,0.5)',
+        'rgba(255,182,193,0.5)'
+    ],
+    borderColor: [
+        'rgba(163,221,203,1)',
+        'rgba(232,233,161,1)',
+        'rgba(230,181,102,1)',
+        'rgba(229,112,126,1)',
+        'rgba(135,206,250,1)',
+        'rgba(255,182,193,1)'
+    ],
+    borderWidth: 1
 };
 
 new Chart(grafica, {
@@ -52,5 +67,9 @@ new Chart(grafica, {
         datasets: [
             datos
         ]
+    },
+      options: {
+        responsive: false, 
+        maintainAspectRatio: false
     }
 });
